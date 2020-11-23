f7d3ccc2c95685ae44c9d590b5c4145531fccbde
tlaritz <tlaritz@earthlink.net>
2020-11-23 12:31:18 -0700

    Added comments describing the two functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,15 @@
+## makeCacheMatrix returns a list with 4 functions.
+## The environment of the list also contains two variables:
+## m, a matrix, and inv, the inverse of matrix m.
+## This function makeCacheMatrix should be called first.
+## Note that the matrix whose inverse is desired may
+## optionally be passed to makeCacheMatrix.
+## The returned list can be used to get and set the matrix m
+## or get or set the the inverse of m.
+## Additionally, the returned list may be passed to
+## cacheSolve which either computes the inverse
+## or, if the inverse of m has already been computed,
+## the cached inverse of m.
 makeCacheMatrix <- function(m = matrix())
 {
   inv <- NULL
@@ -19,6 +31,16 @@ makeCacheMatrix <- function(m = matrix())
        getinv = getinv)
 }
 
+
+##
+## cacheSolve does the hard work of computing the 
+## the inverse, if not already computed, or
+## returning the cached inverse, if the inverse
+## has already be computed.
+## It is important to note that cacheSolve
+## expects as its sole argument a list which
+## is returned from makeCacheMatrix.
+##
 cacheSolve <- function(x)
 {
   inv <- x$getinv()
